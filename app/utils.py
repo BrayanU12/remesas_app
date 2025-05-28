d7adeacbc041127e4dbef707d3a1f028e61b87bd
BrayanU12 <128045649+BrayanU12@users.noreply.github.com>
2025-05-27 21:17:15 -0500

    utils.py

--- app/utils.py
+++ app/utils.py
@@ -1,18 +1,16 @@
-import pandas as pd
-import os
 import json
+import os
 
-def cargar_datos_json(archivo):
-    if not os.path.exists(archivo):
-        return pd.DataFrame()
-    try:
-        with open(archivo, "r", encoding="utf-8") as f:
-            datos = json.load(f)
-        return pd.DataFrame(datos)
-    except json.JSONDecodeError:
-        return pd.DataFrame()
+def cargar_datos_json(ruta):
+    if not os.path.exists(ruta):
+        return []
+    with open(ruta, "r") as archivo:
+        try:
+            return json.load(archivo)
+        except json.JSONDecodeError:
+            return []
 
-def guardar_datos_json(df, archivo):
-    with open(archivo, "w", encoding="utf-8") as f:
-        json.dump(df.to_dict(orient="records"), f, indent=4, ensure_ascii=False)
+def guardar_datos_json(ruta, datos):
+    with open(ruta, "w") as archivo:
+        json.dump(datos, archivo, indent=4)
 
